93967b9a64d6fdf29ea51ceb1ce91c3c7df040b6
Rodriguez Michael Andres <46464840+rodriguezmichaelandres@users.noreply.github.com>
2019-03-29 16:26:09 -0500

    local storage

---
+++ storage.js
@@ -0,0 +1,59 @@
+localStorageShow();
+function localStorageShow()
+{
+	x = new Boolean(false);
+	if(x)
+	{
+		getLocalStorage();
+	}
+	else
+	{
+		saveLocalStorage();
+		getLocalStorage();
+		x = false;
+	}
+		
+}
+
+
+function getLocalStorage()
+{
+	if(localStorage.getItem("nameLS"))
+	{
+	let name = localStorage.getItem("nameLS");
+	let age = localStorage.getItem("ageLS");
+	let email = localStorage.getItem("emailLS");
+	let person = JSON.parse(localStorage.getItem("personLS"));
+	console.log(name);
+	console.log(age);
+	console.log(email);
+	console.log(person);
+
+	document.write("Your data are: ", name, " ", age, " ", email);
+	}
+	else
+	{
+	console.log("Empty");
+	}
+}
+
+function saveLocalStorage()
+{
+
+	let namePerson = prompt("Name");
+	let agePerson = prompt("Age?");
+	let emailPerson = prompt("Email?");
+
+	let person = { 
+	name:namePerson,
+	age:agePerson,
+	email:emailPerson
+	};
+
+
+	localStorage.setItem("nameLS", namePerson);
+	localStorage.setItem("ageLS", agePerson);
+	localStorage.setItem("emailLS", emailPerson);
+	localStorage.setItem("personLS", JSON.stringify(person));
+
+}
