dd2aeba1573b642b980b33062ee613ee21d31015
Rodriguez Michael Andres <46464840+rodriguezmichaelandres@users.noreply.github.com>
2019-03-29 16:23:30 -0500

    Add files via upload

--- storage.js
+++ storage.js
@@ -14,45 +14,46 @@ function localStorageShow()
 	}
 		
 }
+
+
 function getLocalStorage()
 {
-if(localStorage.getItem("nameLS"))
-{
-let name = localStorage.getItem("nameLS");
-let age = localStorage.getItem("ageLS");
-let email = localStorage.getItem("emailLS");
-let person = JSON.parse(localStorage.getItem("personLS"));
-console.log(name);
-console.log(age);
-console.log(email);
-console.log(person);
+	if(localStorage.getItem("nameLS"))
+	{
+	let name = localStorage.getItem("nameLS");
+	let age = localStorage.getItem("ageLS");
+	let email = localStorage.getItem("emailLS");
+	let person = JSON.parse(localStorage.getItem("personLS"));
+	console.log(name);
+	console.log(age);
+	console.log(email);
+	console.log(person);
 
-document.write("Your data are: ", name, " ", age, " ", email);
-}
-else
-{
-console.log("Empty");
-}
+	document.write("Your data are: ", name, " ", age, " ", email);
+	}
+	else
+	{
+	console.log("Empty");
+	}
 }
 
 function saveLocalStorage()
 {
 
-let namePerson = prompt("Name");
-let agePerson = prompt("Age?");
-let emailPerson = prompt("Email?");
+	let namePerson = prompt("Name");
+	let agePerson = prompt("Age?");
+	let emailPerson = prompt("Email?");
 
-let person = { 
-name:namePerson,
-age:agePerson,
-email:emailPerson
-};
+	let person = { 
+	name:namePerson,
+	age:agePerson,
+	email:emailPerson
+	};
 
 
-localStorage.setItem("nameLS", namePerson);
-localStorage.setItem("ageLS", agePerson);
-localStorage.setItem("emailLS", emailPerson);
-localStorage.setItem("personLS", JSON.stringify(person));
-
+	localStorage.setItem("nameLS", namePerson);
+	localStorage.setItem("ageLS", agePerson);
+	localStorage.setItem("emailLS", emailPerson);
+	localStorage.setItem("personLS", JSON.stringify(person));
 
 }
